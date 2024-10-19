7f5be2b8420acfa1586a473210566ad6a1a28fe6
yachiashen <yachiashen2003@gmail.com>
2024-10-19 09:39:11 +0800

    lab1 init

---
+++ lab1/receiver.c
@@ -0,0 +1,19 @@
+#include "receiver.h"
+
+void receive(message_t* message_ptr, mailbox_t* mailbox_ptr){
+    /*  TODO: 
+        1. Use flag to determine the communication method
+        2. According to the communication method, receive the message
+    */
+}
+
+int main(){
+    /*  TODO: 
+        1) Call receive(&message, &mailbox) according to the flow in slide 4
+        2) Measure the total receiving time
+        3) Get the mechanism from command line arguments
+            • e.g. ./receiver 1
+        4) Print information on the console according to the output format
+        5) If the exit message is received, print the total receiving time and terminate the receiver.c
+    */
+}
